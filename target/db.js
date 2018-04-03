886139512237bb4ff3db8cdc60b2800c3a03ddab
evagolabek <golabek.eva@gmail.com>
2018-04-03 20:49:57 +0200

    typeORM add

---
+++ target/db.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+const DefaultNamingStrategy_1 = require("typeorm/naming-strategy/DefaultNamingStrategy");
+const StringUtils_1 = require("typeorm/util/StringUtils");
+const entity_1 = require("./batches/entity");
+const entity_2 = require("./evaluations/entity");
+const entity_3 = require("./students/entity");
+const entity_4 = require("./users/entity");
+class CustomNamingStrategy extends DefaultNamingStrategy_1.DefaultNamingStrategy {
+    tableName(targetName, userSpecifiedName) {
+        return userSpecifiedName ? userSpecifiedName : StringUtils_1.snakeCase(targetName) + 's';
+    }
+    columnName(propertyName, customName, embeddedPrefixes) {
+        return StringUtils_1.snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
+    }
+    columnNameCustomized(customName) {
+        return customName;
+    }
+    relationName(propertyName) {
+        return StringUtils_1.snakeCase(propertyName);
+    }
+}
+exports.default = () => typeorm_1.createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL || 'postgres://localhost:5432/postgres',
+    entities: [
+        entity_4.default,
+        entity_1.default,
+        entity_2.default,
+        entity_3.default,
+    ],
+    synchronize: true,
+    logging: true,
+    namingStrategy: new CustomNamingStrategy()
+})
+    .then(_ => console.log('Connected to Postgres with TypeORM'));
+//# sourceMappingURL=db.js.map
